cb07231c5c9bcebabcbd07388f2b3ac0d7611d21
Maciej Pasternacki <maciej@pasternacki.net>
2012-08-29 16:29:35 +0200

    Use Markdown for README.

---
+++ README.md
@@ -0,0 +1,9 @@
+# DESCRIPTION:
+
+Sets hostname and FQDN of the node.
+
+# ATTRIBUTES:
+
+set_fqdn - FQDN to set
+
+The latest code is hosted at https://github.com/3ofcoins/chef-cookbook-hostname
--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Maciej Pasternacki"
 maintainer_email "maciej@pasternacki.net"
 license          "MIT"
 description      "Configures hostname and FQDN"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
 
 supports "debian"
