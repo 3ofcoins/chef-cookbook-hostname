75ef0cc5257b85e9f9ec9447625474d17a87e3a5
Damien Roche <droche@daxmac.local>
2014-01-28 14:32:36 +0000

    Added supports 'centos' 'redhat'

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,12 @@ maintainer_email "maciej@pasternacki.net"
 license          "MIT"
 description      "Configures hostname and FQDN"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 supports "debian"
 supports "ubuntu"
 supports "freebsd"
+supports "centos"
+supports "redhat"
 
 depends "hostsfile"
