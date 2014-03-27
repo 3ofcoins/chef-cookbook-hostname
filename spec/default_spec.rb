b8e37e906ab9ae846439b0b85b15dd6842e5212d
Marta Paciorkowska <marta@paciorkowska.pl>
2014-03-28 00:44:03 +0100

    Updates deprecated chefspec methods and classes

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -3,14 +3,14 @@
 require 'chefspec'
 
 describe 'hostname::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new }
+  let (:chef_run) { ChefSpec::Runner.new }
 
   it 'sets FQDN' do
     chef_run.node.set['set_fqdn'] = 'test.example.com'
     chef_run.converge 'hostname'
 
-    expect(chef_run).to create_file_with_content '/etc/hostname', "test\n"
-    expect(chef_run).to execute_command 'hostname test'
+    expect(chef_run).to render_file('/etc/hostname').with_content("test\n")
+    expect(chef_run).to run_execute('hostname test')
   end
 
   it "substitutes star to node's name" do
@@ -18,7 +18,7 @@ describe 'hostname::default' do
     chef_run.node.set['set_fqdn'] = '*.example.com'
     chef_run.converge 'hostname'
 
-    expect(chef_run).to create_file_with_content '/etc/hostname', "test\n"
-    expect(chef_run).to execute_command 'hostname test'
+    expect(chef_run).to render_file('/etc/hostname').with_content("test\n")
+    expect(chef_run).to run_execute('hostname test')
   end
 end
--- spec/vmware_spec.rb
+++ spec/vmware_spec.rb
@@ -3,7 +3,7 @@
 require 'chefspec'
 
 describe 'hostname::vmware' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'hostname::vmware' }
+  let (:chef_run) { ChefSpec::Runner.new.converge 'hostname::vmware' }
   it 'should do something' do
     pending 'Your recipe examples go here.'
   end
