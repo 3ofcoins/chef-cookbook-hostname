f1d13d32c619e7c805854314e666680084f00de5
Grant Ridder <shortdudey123@gmail.com>
2016-09-06 19:42:11 -0700

    Clean up deprecation warnings
    Fixes
    ```
    WARN: method access to node attributes (node.foo.bar) is deprecated and will be removed in Chef 13, please use bracket syntax (node["foo"]["bar"])
    ```

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 default['hostname_cookbook']['hostsfile_ip'] = '127.0.1.1'
-default['hostname_cookbook']['hostsfile_ip_interface'] = 'lo0' if platform == 'freebsd'
+default['hostname_cookbook']['hostsfile_ip_interface'] = 'lo0' if node['platform'] == 'freebsd'
 default['hostname_cookbook']['hostsfile_aliases'] = []
 default['hostname_cookbook']['hostsfile_include_hostname_in_aliases'] = true
