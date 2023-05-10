5a0af695439c5464bf56d5771f08118656af3872
Akhil Nayabu <anayabu@salesforce.com>
2023-05-10 22:15:08 +0530

    increasing version number for cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'maciej@3ofcoins.net'
 license          'MIT'
 description      'Configures hostname and FQDN'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.2'
+version          '0.4.3'
 
 supports 'debian'
 supports 'ubuntu'
